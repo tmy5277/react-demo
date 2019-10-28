631f75d1b151e74c1372eed1664d7f5da8f1025f
Tan <mingyu.tan@yff.com>
2019-10-28 14:51:08 +0800

    fix warning

--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -1,47 +1,47 @@
 import 'whatwg-fetch'
 import axios from 'axios'
-import urlHandler from './urlHandler'
+// import urlHandler from './urlHandler'
 
 // fetch
-let fetchData = (url, params = {}, options = {}) => {
-  let defaultOptions = {
-    method: 'GET',
-    credentials: 'include',
-    headers: {
-      'Content-Type': 'application/json',
-      'Accept-Charset': 'utf-8'
-    }
-  }
-  let fetchOptions = { ...defaultOptions, ...options }
-  if (fetchOptions.method === 'get' || fetchOptions.method === 'GET') {
-    let str = urlHandler.obj2query(params)
-    url += str.length ? `?${str}` : ''
-  } else {
-    try {
-      fetchOptions.body = JSON.stringify(params)
-    } catch (e) {
-      console.log(e)
-      console.error('Params must be Object!')
-    }
-  }
-  // console.log(fetchOptions)
-  return new Promise((resolve, reject) => {
-    fetch(url, fetchOptions).then(response => {
-      // 如果返回的状态为200，表示正常
-      if (response.status === 200) {
-        return response.json();
-      } else {
-        // 服务器端抛出的错误会在这里捕获
-        // reject(response.statusText)
-        console.log(response)
-      }
-    }).then(res => {
-      resolve(res)
-    }).catch(err => {
-      reject(err)
-    })
-  })
-}
+// let fetchData = (url, params = {}, options = {}) => {
+//   let defaultOptions = {
+//     method: 'GET',
+//     credentials: 'include',
+//     headers: {
+//       'Content-Type': 'application/json',
+//       'Accept-Charset': 'utf-8'
+//     }
+//   }
+//   let fetchOptions = { ...defaultOptions, ...options }
+//   if (fetchOptions.method === 'get' || fetchOptions.method === 'GET') {
+//     let str = urlHandler.obj2query(params)
+//     url += str.length ? `?${str}` : ''
+//   } else {
+//     try {
+//       fetchOptions.body = JSON.stringify(params)
+//     } catch (e) {
+//       console.log(e)
+//       console.error('Params must be Object!')
+//     }
+//   }
+//   // console.log(fetchOptions)
+//   return new Promise((resolve, reject) => {
+//     fetch(url, fetchOptions).then(response => {
+//       // 如果返回的状态为200，表示正常
+//       if (response.status === 200) {
+//         return response.json();
+//       } else {
+//         // 服务器端抛出的错误会在这里捕获
+//         // reject(response.statusText)
+//         console.log(response)
+//       }
+//     }).then(res => {
+//       resolve(res)
+//     }).catch(err => {
+//       reject(err)
+//     })
+//   })
+// }
 
 // axios
 axios.defaults.headers.post['Content-Type'] = 'application/json'
